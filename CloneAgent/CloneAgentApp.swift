48b633ef2b4c4602a993de6b16ec145044d372e2
Maurice Pfurr <mpdesigncode27@gmail.com>
2026-05-12 23:14:43 +0200

    Initial Commit

---
+++ CloneAgent/CloneAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneAgentApp.swift
+//  CloneAgent
+//
+//  Created by Maurice Pfurr on 12.05.26.
+//
+
+import SwiftUI
+
+@main
+struct CloneAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
